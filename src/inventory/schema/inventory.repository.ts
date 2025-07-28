cc8572bf4ba513165ee487183eadd70d77cf78b8
Ojo Micheal <44380037+trillzglobal@users.noreply.github.com>
2025-07-28 12:18:34 +0100

    feat(inventory): enforce validation on updates

--- src/inventory/schema/inventory.repository.ts
+++ src/inventory/schema/inventory.repository.ts
@@ -109,6 +109,7 @@ export class InventoryRepository {
     async updateInventory(search : any, data : any) {
         return await this.inventoryModel.findOneAndUpdate(search, data, {
             new: true,
+            runValidators: true,
         });
     }
     
